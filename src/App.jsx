6e7f0b70d031234b7b5092bdc1cd406f62b6b3b4
Divesh Keswani <diveshkswn@gmail.com>
2021-07-05 15:47:22 +0530

    Initial commit and push to remote

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React from 'react';
+import './App.css';
+
+// 9 : 00
+function App() {
+  return (
+    <div className="App">
+
+      Hello React app
+
+    </div>
+  );
+}
+
+export default App;
