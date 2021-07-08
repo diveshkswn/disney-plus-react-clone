2438dd135260b61b4af835ec3a8b670fb8f976d0
Divesh Keswani <diveshkswn@gmail.com>
2021-07-08 20:28:40 +0530

    image slider fixes

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -4,15 +4,16 @@ import './ImageSlider.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
-// 1:15
+
 function ImageSlider() {
   const settings = {
-    dots: true,
+
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
+    dots: true,
 
   };
 
