aa74c5451bc086b8e94f3e10703bf130068eecef
Divesh Keswani <diveshkswn@gmail.com>
2021-07-08 19:16:12 +0530

    image slider component completed

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -13,16 +13,28 @@ function ImageSlider() {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
+
   };
 
   return (
     <div className="ImageSlider_main">
       <Slider {...settings}>
+
         <div className="imageSlider_wrap">
-          <img src="/images/slider-badging.jpg" alt="slider_img" />
+          <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/1681/1011681-h-ac6ee255f074" alt="slider_img" className="imageSlider_img" />
+
         </div>
         <div className="imageSlider_wrap">
-          <img src="/images/slider-badag.jpg" alt="slider_img" />
+          <img src="/images/slider-badging.jpg" alt="slider_img" className="imageSlider_img" />
+        </div>
+        <div className="imageSlider_wrap">
+          <img src="/images/slider-badag.jpg" alt="slider_img" className="imageSlider_img" />
+        </div>
+        <div className="imageSlider_wrap">
+          <img src="/images/slider-scale.jpg" alt="slider_img" className="imageSlider_img" />
+        </div>
+        <div className="imageSlider_wrap">
+          <img src="/images/slider-scales.jpg" alt="slider_img" className="imageSlider_img" />
         </div>
       </Slider>
 
