4dab43b2cf6f85dee65105a8deb7dd202bd52576
Divesh Keswani <diveshkswn@gmail.com>
2021-07-10 00:06:56 +0530

    Detials component: Partially done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/pages/Home';
+import Detail from './components/Detail';
 
 function App() {
   return (
+
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        {/* Navbar */}
+        <Header />
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/detail" component={Detail} />
+
+          {/* Default path */}
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Router>
     </div>
+
   );
 }
 
--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,10 +1,33 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 import './Detail.css';
-
+// 2:37
 function Detail() {
   return (
     <div className="detail_main">
-      Detail
+      <div className="detail_bg">
+        <img src="https://images.indianexpress.com/2021/05/loki-1200.jpg" alt="" />
+      </div>
+      <div className="detail_title">
+        <img src="/images/loki_png.png" alt="" />
+      </div>
+      <div className="details_controls">
+        <button className="details_playButton">
+          <img src="/images/play-icon-black.png" alt="" />
+          <span>PLAY</span>
+        </button>
+        <button className="details_trailerButton">
+          <img src="/images/play-icon-white.png" alt="" />
+          <span>TRAILER</span>
+        </button>
+        <button className="details_addButton">
+          <span>+</span>
+        </button>
+        <button className="details_grpWatchButton">
+          <img src="/images/group-icon.png" alt="" />
+        </button>
+      </div>
     </div>
   );
 }
